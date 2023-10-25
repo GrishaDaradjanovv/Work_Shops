1ceb9af08fa6c0164111c738445175fa75214d2a
GrishaDaradzhanov <daradjanov@gmail.com>
2023-10-25 19:22:59 +0300

    Create Commands done

--- WorkShop2/src/com/company/oop/cosmetics/commands/CreateShampooCommand.java
+++ WorkShop2/src/com/company/oop/cosmetics/commands/CreateShampooCommand.java
@@ -2,13 +2,18 @@ package com.company.oop.cosmetics.commands;
 
 import com.company.oop.cosmetics.core.contracts.Command;
 import com.company.oop.cosmetics.core.contracts.CosmeticsRepository;
+import com.company.oop.cosmetics.models.enums.GenderType;
+import com.company.oop.cosmetics.models.enums.UsageType;
+import com.company.oop.cosmetics.utils.ParsingHelpers;
+import com.company.oop.cosmetics.utils.ValidationHelpers;
 
 import java.util.List;
+
+import static com.company.oop.cosmetics.utils.ParsingHelpers.*;
 
 public class CreateShampooCommand implements Command {
 
     public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 6;
-
     private final CosmeticsRepository cosmeticsRepository;
 
     public CreateShampooCommand(CosmeticsRepository cosmeticsRepository) {
@@ -17,7 +22,24 @@ public class CreateShampooCommand implements Command {
 
     @Override
     public String execute(List<String> parameters) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+        String shampooName = parameters.get(0);
+        String shampooBrand = parameters.get(1);
+        double shampooPrice = ParsingHelpers.tryParseDouble(parameters.get(2),ParsingHelpers.INVALID_PRICE);
+        GenderType genderType = ParsingHelpers.tryParseGender(parameters.get(3));
+        int milliliters = ParsingHelpers.tryParseInt(parameters.get(4),INVALID_MILLILITRES);
+        UsageType usageType =ParsingHelpers.tryParseUsageType(parameters.get(5));
+
+        return createShampoo(shampooName,shampooBrand,shampooPrice,genderType,milliliters,usageType);
+    }
+
+    private String createShampoo(String shampooName, String shampooBrand,double shampooPrice,GenderType genderType
+            ,int milliliters,UsageType usageType) {
+        if (cosmeticsRepository.productExist(shampooName)){
+            throw new IllegalArgumentException(String.format(PRODUCT_NAME_ALREADY_EXISTS,"Shampoo",shampooName));
+        }
+        cosmeticsRepository.createShampoo(shampooName,shampooBrand,shampooPrice,genderType,milliliters,usageType);
+        return  String.format(PRODUCT_CREATED,"Shampoo",shampooName);
     }
 
 }
--- WorkShop2/src/com/company/oop/cosmetics/commands/CreateToothpasteCommand.java
+++ WorkShop2/src/com/company/oop/cosmetics/commands/CreateToothpasteCommand.java
@@ -2,8 +2,14 @@ package com.company.oop.cosmetics.commands;
 
 import com.company.oop.cosmetics.core.contracts.Command;
 import com.company.oop.cosmetics.core.contracts.CosmeticsRepository;
+import com.company.oop.cosmetics.models.enums.GenderType;
+import com.company.oop.cosmetics.utils.ParsingHelpers;
 
+import java.util.Arrays;
 import java.util.List;
+
+import static com.company.oop.cosmetics.utils.ParsingHelpers.PRODUCT_CREATED;
+import static com.company.oop.cosmetics.utils.ParsingHelpers.PRODUCT_NAME_ALREADY_EXISTS;
 
 public class CreateToothpasteCommand implements Command {
 
@@ -17,7 +23,24 @@ public class CreateToothpasteCommand implements Command {
 
     @Override
     public String execute(List<String> parameters) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        String toothpasteName = parameters.get(0);
+        String toothpasteBrandName = parameters.get(1);
+        double toothpastePrice = ParsingHelpers.tryParseDouble(parameters.get(2), ParsingHelpers.INVALID_PRICE);
+        GenderType genderType = ParsingHelpers.tryParseGender(parameters.get(3));
+        String ingredients = parameters.get(4);
+        List<String> ingredientsList = Arrays.asList(ingredients.split(","));
+
+        return createToothpaste(toothpasteName,toothpasteBrandName,toothpastePrice,genderType,ingredientsList);
+    }
+
+    private String createToothpaste(String toothpasteName, String toothpasteBrandName, double toothpastePrice,
+                                    GenderType genderType,List<String>ingredientsList){
+        if (cosmeticsRepository.productExist(toothpasteName)){
+            throw new IllegalArgumentException(String.format(PRODUCT_NAME_ALREADY_EXISTS,"Toothpaste", toothpasteName));
+        }
+        cosmeticsRepository.createToothpaste(toothpasteName,toothpasteBrandName,toothpastePrice,genderType
+                ,ingredientsList);
+        return String.format(PRODUCT_CREATED,"Toothpaste",toothpasteName);
     }
 
 }
