d68052a4309b7a0871410c7d8721158ea042bd65
GrishaDaradzhanov <daradjanov@gmail.com>
2023-10-25 18:13:40 +0300

    ToothpasteImpl finished.

--- WorkShop2/src/com/company/oop/cosmetics/models/ToothpasteImpl.java
+++ WorkShop2/src/com/company/oop/cosmetics/models/ToothpasteImpl.java
@@ -2,9 +2,12 @@ package com.company.oop.cosmetics.models;
 
 import com.company.oop.cosmetics.models.contracts.Toothpaste;
 import com.company.oop.cosmetics.models.enums.GenderType;
+import com.company.oop.cosmetics.utils.ValidationHelpers;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.company.oop.cosmetics.utils.ParsingHelpers.INVALID_PRICE;
 
 public class ToothpasteImpl extends ProductImpl implements Toothpaste {
 
@@ -15,32 +18,42 @@ public class ToothpasteImpl extends ProductImpl implements Toothpaste {
     private List<String> ingredients;
 
     public ToothpasteImpl(String name, String brandName, double price, GenderType genderType, List<String> ingredients) {
-        super(name,brandName,price,genderType);
-        this.ingredients = new ArrayList<>();
+        super(name, brandName, price, genderType);
+        this.ingredients = ingredients;
     }
 
     @Override
     protected void nameValidation(String name) {
-
+        ValidationHelpers.validateStringLength(name, NAME_MIN_LENGTH, NAME_MAX_LENGTH, "Name");
     }
 
     @Override
     protected void brandNameValidation(String brandName) {
-
+        ValidationHelpers.validateStringLength(brandName, BRAND_NAME_MIN_LENGTH, BRAND_NAME_MAX_LENGTH, "Brand name");
     }
 
     @Override
     protected void priceValidation(double price) {
-
+        if (price < 0) {
+            throw new IllegalArgumentException(INVALID_PRICE);
+        }
     }
 
     @Override
     public List<String> getIngredients() {
-        return null;
+        return new ArrayList<>(ingredients);
     }
 
-
-
+    @Override
+    public String print() {
+    List<String>ingredients = getIngredients();
+    String ingr =String.join("," ,ingredients);
+        return String.format("""
+                 %s
+                Ingredients: [%s]
+                """,super.print(),ingr);
+    }
+//     #Ingredients: [VALUE, VALUE]
     /* This method should be uncommented when you are done with the class.
     @Override
     public boolean equals(Object o) {
